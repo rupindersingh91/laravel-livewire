c8945a77d5ff8f9455880c40954648993a0d7836
Rupinder Singh <rupindersingh0853@gmail.com>
2023-11-05 12:32:39 +0530

    Comments Added to code

--- app/Http/Livewire/CreatePoll.php
+++ app/Http/Livewire/CreatePoll.php
@@ -9,19 +9,37 @@ class CreatePoll extends Component
     public $title;
     public $options = ['First'];
 
+    /**
+     * Render the view for creating a poll.
+     *
+     * @return Illuminate\View\View
+     */
     public function render()
     {
+        // Return the view for creating a poll using the "livewire.create-poll" template.
         return view('livewire.create-poll');
     }
 
+    /**
+     * Adds an option to the options array.
+     */
     public function addOption()
     {
         $this->options[] = '';
     }
 
+    /**
+     * Removes an option from the list of options at the given index.
+     *
+     * @param int $index The index of the option to remove.
+     * @return void
+     */
     public function removeOption($index)
     {
+        // Unset the option at the given index
         unset($this->options[$index]);
+
+        // Re-index the options array
         $this->options = array_values($this->options);
     }
 }
