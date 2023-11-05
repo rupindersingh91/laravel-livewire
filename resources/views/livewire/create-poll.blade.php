94723174878c9893094de2328c253138145cd8d8
Rupinder Singh <rupindersingh0853@gmail.com>
2023-11-05 12:08:33 +0530

    Add options added.

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -4,5 +4,17 @@
         <input type="text" wire:model="title" />
 
         Current title: {{$title}}
+
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption"> Add Option </button>
+        </div>
+
+        <div class="">
+            @foreach($options as $index => $option)
+            <div class="mb-4">
+                {{$index}} - {{$option}}
+            </div>
+            @endforeach
+        </div>
     </form>
 </div>
